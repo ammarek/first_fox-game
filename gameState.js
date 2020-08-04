1f5fd3d09e3426a833ed867104a3b7e7eb50d007
AK77 <AK77@Ammar-Kutiyanawallas-MacBook-Pro.local>
2020-08-04 12:52:16 +0530

    changed folder names to make website work

--- gameState.js
+++ gameState.js
@@ -1,4 +1,4 @@
-import { modFox, modScene, togglePoopBag, writeModal, } from "./ui";
+import { modFox, modScene, togglePoopBag, writeModal } from "ui.js";
 import {
   RAIN_CHANCE,
   SCENES,
@@ -7,7 +7,7 @@ import {
   getNextHungerTime,
   getNextDieTime,
   getNextPoopTime,
-} from "./constants";
+} from "constants.js";
 
 const gameState = {
   current: "INIT",
@@ -32,13 +32,13 @@ const gameState = {
     } else if (this.clock === this.dieTime) {
       this.die();
     } else if (this.clock === this.timeToStartCelebrating) {
-        this.startCelebrating();
+      this.startCelebrating();
     } else if (this.clock === this.timeToEndCelebrating) {
-        this.endCelebrating();
+      this.endCelebrating();
     } else if (this.clock === this.poopTime) {
-        this.poop();
+      this.poop();
     }
- 
+
     return this.clock;
   },
   startGame() {
@@ -65,13 +65,13 @@ const gameState = {
     this.wakeTime = this.clock + NIGHT_LENGTH;
   },
   clearTimes() {
-      this.wakeTime = -1;
-      this.sleepTime = -1;
-      this.hungryTime = -1;
-      this.dieTime = -1;
-      this.poopTime = -1;
-      this.timeToStartCelebrating = -1;
-      this.timetoEndCelebrating = -1;
+    this.wakeTime = -1;
+    this.sleepTime = -1;
+    this.hungryTime = -1;
+    this.dieTime = -1;
+    this.poopTime = -1;
+    this.timeToStartCelebrating = -1;
+    this.timetoEndCelebrating = -1;
   },
   getHungry() {
     this.current = "HUNGRY";
@@ -80,39 +80,38 @@ const gameState = {
     modFox("hungry");
   },
   poop() {
-      this.current = "POOPING";
-      this.poopTime = -1;
-      this.dieTime = getNextDieTime(this.clock);
-      modFox("pooping");
+    this.current = "POOPING";
+    this.poopTime = -1;
+    this.dieTime = getNextDieTime(this.clock);
+    modFox("pooping");
   },
   die() {
     this.current = "DEAD";
-    modScene('dead');
-    modFox('dead'),
-    this.clearTimes();
-    writeModal ("The fox died :( <br/> Press the middle button to start");
+    modScene("dead");
+    modFox("dead"), this.clearTimes();
+    writeModal("The fox died :( <br/> Press the middle button to start");
   },
   startCelebrating() {
-      this.current = "CELEBRATING";
-      modFox('celebrate');
-      this.timeToStartCelebrating = -1;
-      this.timeToEndCelebrating = this.clock + 2;
+    this.current = "CELEBRATING";
+    modFox("celebrate");
+    this.timeToStartCelebrating = -1;
+    this.timeToEndCelebrating = this.clock + 2;
   },
   endCelebrating() {
-      this.timetoEndCelebrating = -1
-      this.current = "IDLING";
-      this.determineFoxState();
-      togglePoopBag(false);
+    this.timetoEndCelebrating = -1;
+    this.current = "IDLING";
+    this.determineFoxState();
+    togglePoopBag(false);
   },
   determineFoxState() {
-      if (this.current === 'IDLING') {
-          if  (SCENES[this.scene] === 'rain') {
-          modFox('rain');
-        } else {
-          modFox('idling');
-        }
-        }
-    },
+    if (this.current === "IDLING") {
+      if (SCENES[this.scene] === "rain") {
+        modFox("rain");
+      } else {
+        modFox("idling");
+      }
+    }
+  },
   handleUserAction(icon) {
     if (
       ["SLEEP", "FEEDING", "CELEBRATING", "HATCHING"].includes(this.current)
@@ -145,7 +144,7 @@ const gameState = {
   },
   cleanUpPoop() {
     if (!this.current === "POOPING") {
-        return;
+      return;
     }
 
     this.dieTime = -1;
--- init.js
+++ init.js
@@ -1,27 +1,25 @@
-import gameState, { handleUserAction } from './gameState';
-import { TICK_RATE } from './constants';
-import initButtons from './buttons'; 
+import gameState, { handleUserAction } from "gameState";
+import { TICK_RATE } from "constants";
+import initButtons from "buttons";
 
-async function init (){
-    console.log('starting game');
-    initButtons(handleUserAction);
+async function init() {
+  console.log("starting game");
+  initButtons(handleUserAction);
 
-    let nextTimeToTick = Date.now();
+  let nextTimeToTick = Date.now();
 
-    function nextAnimationFrame() {
-        const now = Date.now();
+  function nextAnimationFrame() {
+    const now = Date.now();
 
-        if (nextTimeToTick <= now){
-            gameState.tick();
-            nextTimeToTick = now + TICK_RATE;
-
-        }
-
-        requestAnimationFrame(nextAnimationFrame);
+    if (nextTimeToTick <= now) {
+      gameState.tick();
+      nextTimeToTick = now + TICK_RATE;
     }
 
     requestAnimationFrame(nextAnimationFrame);
+  }
 
+  requestAnimationFrame(nextAnimationFrame);
 }
 
 init();
